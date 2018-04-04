5edfa92be3c010cb5f70c2a9c089ebb4fad08c69
VA <37845725+andvlad@users.noreply.github.com>
2018-04-04 16:50:51 +0400

    New changes

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<script src="js/ontop.js"></script>
+		<div class="top">
+      		<a href="https://github.com/andvlad/code-practice" id="bottomtext">Андрюшин В.О., 2018</a>
+      		<?php require_once("contact_form.html"); ?>
+    	</div>
+	</body>
+</html>
